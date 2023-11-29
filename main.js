574ff4f20ad4c48a84794c4e61e0baa591bb3f65
ruhidnovruzov <ruhidnovruzov2@gmail.com>
2023-11-29 17:32:55 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// Task-1
+// var number=99
+// function swapNumber(){
+// var stringNumber=number.toString()
+// var swappedNumber= +stringNumber.split('').reverse().join('')
+// console.log(swappedNumber<=number)
+// }
+
+// swapNumber()
+
+//Task-2
+
+// function findHighest(number){
+//     var stringNumber=number.toString()
+//     var arrayNumber=stringNumber.split('')
+//     var highestNumber=Math.max(... arrayNumber)
+    
+//     console.log(highestNumber)
+// }
+
+
+// findHighest(379)
+
+//Task-3
+
+// function removeNull(array){
+//     var remove= array.filter(function(element){
+//         return element !==null
+//      })
+ 
+//      console.log(remove)
+// }
+
+// removeNull(["a", null, 5, "b", null, 2, "c"])
+
+//Task-4
+
+// document.querySelector("#grey").addEventListener("click", function() {
+//     document.body.style.backgroundColor = "grey";
+// })
+// document.querySelector("#red").addEventListener("click", function() {
+//     document.body.style.backgroundColor = "red";
+// })
+// document.querySelector("#blue").addEventListener("click", function() {
+//     document.body.style.backgroundColor = "blue";
+// })
+// document.querySelector("#yellow").addEventListener("click", function() {
+//     document.body.style.backgroundColor = "yellow";
+// })
+
+//Task-5
+
+// var name= prompt("Adınızı daxil edin:")
+// var surname= prompt("Soyadınızı daxil edin:")
+// var username= prompt("İstifadəçi adınızı daxil edin:")
+
+// document.querySelector("#blue").style.color="blue"
+// document.querySelector("#red").style.color="red"
+// document.querySelector("#green").style.color="green"
+
+// document.querySelector("#name").innerHTML=name
+// document.querySelector("#surname").innerHTML=surname
+// document.querySelector("#username").innerHTML=username
